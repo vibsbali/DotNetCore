82dc8500ec353d10a80a852c43bfd1330cd901df
Vaibhav Bali <vibsbali@gmail.com>
2016-04-16 14:18:08 +1000

    Added in-direction by using IOC and extracting the Configuration properties to another class

---
+++ src/OdeToFood/Services/Greeter.cs
@@ -0,0 +1,10 @@
+﻿namespace OdeToFood.Services
+{
+    public class Greeter : IGreeter
+    {
+        public string GetGreeting()
+        {
+            return "Hello from the greeter!";
+        }
+    }
+}
