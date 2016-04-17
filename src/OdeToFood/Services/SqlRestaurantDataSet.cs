d39ec59c69e45dbc5b8722d6bb62d7cd10ec40a6
Vaibhav Bali <vibsbali@gmail.com>
2016-04-17 10:42:10 +1000

    Added concrete implementation of IRestaurantData that uses EF's DbContext

---
+++ src/OdeToFood/Services/SqlRestaurantDataSet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OdeToFood.Entities;
+
+namespace OdeToFood.Services
+{
+    public class SqlRestaurantDataSet : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext odeToFoodDbContext;
+
+        public SqlRestaurantDataSet(OdeToFoodDbContext dbContext)
+        {
+            odeToFoodDbContext = dbContext;
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return odeToFoodDbContext.Restaurants;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return odeToFoodDbContext.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public void Add(Restaurant restaurant)
+        {
+            odeToFoodDbContext.Add(restaurant);
+            odeToFoodDbContext.SaveChanges();
+        }
+    }
+}
+
