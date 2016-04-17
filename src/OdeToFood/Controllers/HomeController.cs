6b50ef348df45e849a2d14b2a8ff1288bb4ea85b
Vaibhav Bali <vibsbali@gmail.com>
2016-04-17 13:54:51 +1000

    Removed reference to GreetingService from HomeController and Index View

--- src/OdeToFood/Controllers/HomeController.cs
+++ src/OdeToFood/Controllers/HomeController.cs
@@ -9,20 +9,17 @@ namespace OdeToFood.Controllers
     public class HomeController : Controller
     {
         private readonly IRestaurantData restaurantData;
-        private IGreeter greeter;
 
-        public HomeController(IRestaurantData restaurantData, IGreeter greeter)
+        public HomeController(IRestaurantData restaurantData)
         {
             this.restaurantData = restaurantData;
-            this.greeter = greeter;
         }
 
         public IActionResult Index()
         {
             var model = new HomePageViewModel
             {
-                Restaurants = restaurantData.GetAll(),
-                CurrentGreeting = greeter.GetGreeting()
+                Restaurants = restaurantData.GetAll()
             };
 
             return View(model);
