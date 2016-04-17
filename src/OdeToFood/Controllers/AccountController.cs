701c533c4c0f8361ca6dd3a18187c6fbe358213f
Vaibhav Bali <vibsbali@gmail.com>
2016-04-17 16:47:27 +1000

    Created a ViewModel for Account Registration

---
+++ src/OdeToFood/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ src/OdeToFood/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFood.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required, MaxLength(256)]
+        public string UserName { get; set; }
+
+        [Required, DataType((DataType.Password))]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password)), DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
